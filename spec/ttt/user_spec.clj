2c51eb552781d159a24acbc1aa4bd72f1c280b9d
pwdd <ainapinto@gmail.com>
2016-06-15 09:09:09 -0500

    user

--- spec/ttt/user_spec.clj
+++ spec/ttt/user_spec.clj
@@ -1,3 +1,9 @@
 (ns ttt.user-spec
   (:require [speclj.core :refer :all]
             [ttt.user :refer :all]))
+
+(describe "get-spot"
+  (it "returns 0 if input is '1'"
+    (should= 0 (with-in-str "1" (get-spot))))
+  (it "returns 8 if input is 9"
+    (should= 8 (with-in-str "9" (get-spot)))))
