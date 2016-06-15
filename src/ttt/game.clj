cc41631a152c19322427697306e1a344924dc995
pwdd <ainapinto@gmail.com>
2016-06-15 11:32:35 -0500

    fix core and game

--- src/ttt/game.clj
+++ src/ttt/game.clj
@@ -6,14 +6,12 @@
 (def first-player :x)
 (def second-player :o)
 
+; TODO test
 (defn play
   [board current-player opponent]
   (let [input (player/get-valid-input board)
         game-board (board/move board current-player input)]
     (println (messenger/print-board game-board))
-    (if (board/game-over? game-board)
-      (println (messenger/result game-board))
-      (recur game-board opponent current-player)
-    )
-  )
-)
+    (if (board/game-over? game-board current-player opponent)
+      (println (messenger/result game-board current-player opponent))
+      (recur game-board opponent current-player))))
