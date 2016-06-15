1607467627dc394cb182cf8c736c106170c5795c
pwdd <ainapinto@gmail.com>
2016-06-15 13:28:37 -0500

    test existence of constants holding players markers

--- spec/ttt/game_spec.clj
+++ spec/ttt/game_spec.clj
@@ -1,3 +1,11 @@
 (ns ttt.game-spec
   (require [speclj.core :refer :all]
            [ttt.game :refer :all]))
+
+(describe "first-player"
+  (it "holds first player marker"
+    (should-not (nil? first-player))))
+
+(describe "second-player"
+  (it "holds second player marker"
+    (should-not (nil? second-player))))
