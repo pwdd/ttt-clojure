4c46ec2f07af7407c3d6d709c3d1de44beb3824a
pwdd <ainapinto@gmail.com>
2016-07-05 21:59:08 -0500

    change spots test and fix negamax depth problem

--- spec/ttt/spots_spec.clj
+++ spec/ttt/spots_spec.clj
@@ -1,18 +1,25 @@
 (ns ttt.spots-spec
   (:require [speclj.core :refer :all]
             [ttt.spots :refer :all]
-            [ttt.player :refer [make-player]]))
+            [ttt.player :refer [make-player]]
+            [ttt.board :refer [new-board available-spots]]))
 
 (def human (make-player { :role :human :marker :x }))
 (def easy-computer (make-player { :role :easy-computer :marker :o }))
 
-(describe "select-spot :easy-computer"
-  (it "returns an integer from 0 to board-length exclusive"
-   (should (and (>= (select-spot easy-computer { :board-length 9 }) 0)
-                (< (select-spot easy-computer { :board-length 9 }) 9)))))
-
-(describe "select-spot :human"
-  (it "returns 0 if input is '1'"
-    (should= 0 (with-in-str "1" (select-spot human))))
-  (it "returns 8 if input is 9"
-    (should= 8 (with-in-str "9" (select-spot human)))))
+(describe "select-spot"
+  (context ":human"
+    (it "returns an integer"
+      (should= 0 (with-in-str "1"
+                 (select-spot human { :board (new-board) }))))
+    (it "returns an integer that numeric string minus one"
+      (should= 3 (with-in-str "4"
+                 (select-spot human { :board (new-board) })))))
+  (context ":easy-computer"
+    (with spots (vec (available-spots [:x :_ :o :_ :o :_ :_ :x :o :o])))
+    (it "returns a random index from the available-spots"
+      (should (some #{ (select-spot easy-computer
+                                    { :board [:x :_ :o
+                                              :_ :o :_
+                                              :_ :x :o] })}
+                    @spots)))))
